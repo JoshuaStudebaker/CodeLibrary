1cd070240a1fc965013290fe9d5a177ac645eca9
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-10-13 14:32:31 -0600

    book model

--- obj/Models/Book.cs
+++ obj/Models/Book.cs
@@ -1,7 +1,23 @@
 namespace Name
 {
-  class Book
+  // NOTE maybe make this abstract
+  // REVIEW Why was this put as public and not in the Blockbuster example?
+  public class Book
   {
+
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Description { get; set; }
+    public bool IsAvailable { get; set; }
+
+    public Book(string title, string author, string description, bool IsAvailable)
+    {
+      Title = title;
+      Author - author;
+      Description = description;
+      IsAvailable = true;
+
+    }
 
   }
 }
