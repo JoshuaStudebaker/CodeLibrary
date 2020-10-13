81663a2662832a99c4e0832fd3a822f2a5d3a28b
JoshuaStudebaker <j.s.studebaker1@gmail.com>
2020-10-13 16:31:16 -0600

    some stuff to the service

--- obj/Controllers/LibraryController.cs
+++ obj/Controllers/LibraryController.cs
@@ -1,88 +1,90 @@
 using System;
 // using Library.Services
 
-namespace Library.Controllers
+namespace CodeLibrary.Controllers
 {
-  private LibraryService _Service { get; set; } = new LibraryService();
-  public bool _Running { get; set; } = true;
-  public void Run()
+  // private LibraryService _Service { get; set; } = new LibraryService();
+  class LibraryController
   {
-    while (_Running)
+    public bool _Running { get; set; } = true;
+    public void Run()
     {
-      GetUserInput();
-      Console.WriteLine("Options:\n (A)dd, (R)ead, (C)heckout, Re(t)urn, (D)elete, (Q)uit");
-      string userInput = Console.ReadLine().ToLower();
-      Console.Clear();
-      switch (userInput)
+      while (_Running)
       {
-        case "add":
-        case "a":
-          Add();
-          break;
+        GetUserInput();
+        Console.WriteLine("Options:\n (A)dd, (R)ead, (C)heckout, Re(t)urn, (D)elete, (Q)uit");
+        string userInput = Console.ReadLine().ToLower();
+        Console.Clear();
+        switch (userInput)
+        {
+          case "add":
+          case "a":
+            Add();
+            break;
 
-        case "read":
-        case "r":
-          Read();
-          break;
+          case "read":
+          case "r":
+            Read();
+            break;
 
-        case "checkout":
-        case "c":
-          Checkout();
-          break;
+          case "checkout":
+          case "c":
+            Checkout();
+            break;
 
-        case "return":
-        case "e":
-          Return();
-          break;
+          case "return":
+          case "e":
+            Return();
+            break;
 
-        case "delete":
-        case "d":
-          Delete();
-          break;
+          case "delete":
+          case "d":
+            Delete();
+            break;
 
-        case "quit":
-        case "q":
-          Quit();
-          break;
+          case "quit":
+          case "q":
+            Quit();
+            break;
+        }
+
       }
+    }
+
+    private void GetUserInput()
+    {
+
+    }
+
+    private void Add()
+    {
+
+    }
+
+    private void Read()
+    {
+
+    }
+
+    private void Checkout()
+    {
+
+    }
+
+    private void Return()
+    {
+
+    }
+
+    private void Delete()
+    {
+
+    }
+
+    private void Quit()
+    {
 
     }
   }
-
-  private void GetUserInput()
-  {
-
-  }
-
-  private void Add()
-  {
-
-  }
-
-  private void Read()
-  {
-
-  }
-
-  private void Checkout()
-  {
-
-  }
-
-  private void Return()
-  {
-
-  }
-
-  private void Delete()
-  {
-
-  }
-
-  private void Quit()
-  {
-
-  }
-
 
 }
--- obj/Services/LibraryService.cs
+++ obj/Services/LibraryService.cs
@@ -1,2 +1,36 @@
 using System;
-using B
+using System.Collections.Generic;
+using CodeLibrary.Models;
+
+namespace CodeLibrary.Services
+{
+  class LibraryService
+  {
+
+    private List<Book> Books { get; set; }
+
+    public string GetBooks(bool available)
+    {
+      string list = "";
+      for (int i = 0; i < Books.Count; i++)
+      {
+        var book = Books[i];
+        if (book.IsAvailable == available)
+        {
+          list += $"{i + 1}. {book.Title} - by {book.Author}\n";
+        }
+      }
+      return list;
+    }
+  }
+  private void Checkout()
+  {
+    Console.WriteLine(_service.GetBooks(true));
+  }
+
+  private void Return()
+  {
+    Console.WriteLine(_service.GetBooks(false));
+  }
+
+}
